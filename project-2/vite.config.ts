4694b1d79a7737dbdaba570705d0195dc824272a
Lavana V <lavanavelusamy05112004@gmail.com>
2025-05-21 13:12:14 +0530

    vite

--- project-2/vite.config.ts
+++ project-2/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',   // 👈 important for deployed environments
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
